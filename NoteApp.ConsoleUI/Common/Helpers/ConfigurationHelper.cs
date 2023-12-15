cc86ec5091f22e5c936030a1b0768c36de1f3b91
OlexanderDOl <solenogo1234@gmail.com>
2023-12-15 18:29:03 +0200

    PAN-179:Построить базовый интерфейс для взаимодействия с программой

---
+++ NoteApp.ConsoleUI/Common/Helpers/ConfigurationHelper.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoteApp.ConsoleUI.Common.Helpers
+{
+    static class ConfigurationHelper
+    {
+        public static  IConfigurationRoot SetupConfiguration()
+        {
+            var builder = new ConfigurationBuilder();
+
+            builder.SetBasePath(Directory.GetCurrentDirectory());
+
+            builder.AddJsonFile("appsettings.json");
+
+            return builder.Build();
+
+        }
+    }
+}
--- NoteApp.ConsoleUI/Program.cs
+++ NoteApp.ConsoleUI/Program.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore.Storage;
 using Microsoft.Extensions.Configuration;
 using NoteApp.BusinessLogic.Services;
+using NoteApp.ConsoleUI.Common.Helpers;
 using NoteApp.ConsoleUI.Controllers;
 using NoteApp.DataAccess.Data;
 using NoteApp.DataAccess.Data.Models;
@@ -11,48 +12,121 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        var builder = new ConfigurationBuilder();
-
-        builder.SetBasePath(Directory.GetCurrentDirectory());
-
-        builder.AddJsonFile("appsettings.json");
-
-        var config = builder.Build();
+        var config = ConfigurationHelper.SetupConfiguration();
 
         var connectionString = config.GetConnectionString("DefaultConnetion");
-
-        // чекай фичу
 
         var optionsBuilder = new DbContextOptionsBuilder<TestContext>();
         var options = optionsBuilder.UseSqlite(connectionString).Options;
 
         using var context = new TestContext(options);
-
-        context.Database.EnsureDeleted();
-        context.Database.EnsureCreated();
-        Console.WriteLine("Hello, World!");
         var noteRepository = new NoteRepository(context);
         var noteService = new NoteService(noteRepository);
         var testController = new NoteController(noteService);
-        Note note1 = new Note { Title = "Help", Content = "Puka" };
-        Note note2 = new Note { Title = "Pika3", Content = "FSF" };
-
-        testController.AddNote(note1);
-        testController.AddNote(note2);
-
-        var result = testController.GetNote(1);
-        Console.WriteLine($"{result.Id}:{result.Content}-{ result.Title}");
-
-        note2.Title ="New";
-        note2.Content = "New";
-        testController.UpdateNote(note2);
-
-        testController.RemoveNote(note1);
-
-        var notes = testController.GetAllNotes();
-        foreach (var note in notes)
+        context.Database.EnsureDeleted();
+        context.Database.EnsureCreated();
+        do
         {
-            Console.WriteLine($"{note.Id} : {note.Title} {note.Content}");
-        }
+            Console.WriteLine(" # # # # # Notes App # # # # #");
+            Console.WriteLine("1 - View Notes | 2 - Create Note | 3 - Update Note | 4 - Delete Note | 0 - Exit");
+
+            int menu;
+            Int32.TryParse(Console.ReadLine(), out menu);
+            Console.Clear();
+
+            if (menu == 1)
+            {
+                Console.WriteLine("Your notes");
+                var notes = testController.GetAllNotes();
+                
+                foreach (var note in notes)
+                {
+                    Console.WriteLine($"ID: {note.Id}\n" +
+                  $"Title: {note.Title}\n" +
+                  $"Content: {note.Content}");
+
+                }
+                if(notes.Count != 0)
+                {
+                    Console.WriteLine("Operation succesful");
+                }
+                Console.ReadLine();
+
+            }
+            else if (menu == 2)
+            {
+                Console.WriteLine("Creating new note");
+                Note note = new Note();
+
+                Console.WriteLine("Enter Id");
+                int id;
+                Int32.TryParse(Console.ReadLine(), out id);
+                note.Id = id;
+                Console.WriteLine("Enter Title");
+                note.Title = Convert.ToString(Console.ReadLine());
+                Console.WriteLine("Enter Content");
+                note.Content = Convert.ToString(Console.ReadLine());
+
+                testController.AddNote(note);
+                Console.WriteLine("Operation succesful");
+                Console.ReadLine();
+            }
+            else if (menu == 3)
+            {
+                Console.WriteLine("Update your Note");
+                var notes = testController.GetAllNotes();
+
+                foreach (var note in notes)
+                {
+                    Console.WriteLine($"Existing notes\n:ID: {note.Id}");
+                }
+                Console.WriteLine("Enter Note Id");
+                int id;
+                Int32.TryParse(Console.ReadLine(), out id);
+
+                Note existingNote = testController.GetNote(id);
+                if (existingNote != null)
+                {
+                    Note updatedNote = new Note();
+                    updatedNote.Id = id;
+
+                    Console.WriteLine("Enter new title:");
+                    updatedNote.Title = Convert.ToString(Console.ReadLine());
+                    Console.WriteLine("Enter new content:");
+                    updatedNote.Content = Convert.ToString(Console.ReadLine());
+                    testController.UpdateNote(updatedNote);
+                    Console.WriteLine("Operation succesful");
+                }
+                else
+                {
+                    Console.WriteLine("Note not found!");
+                }
+            }
+            else if (menu == 4)
+            {
+                Console.WriteLine("Delete your Note");
+                Console.WriteLine("Enter Note ID");
+                int id;
+                Int32.TryParse(Console.ReadLine(), out id);
+
+                Note note = testController.GetNote(id);
+
+                if (note != null)
+                {
+                    testController.RemoveNote(note);
+                    Console.WriteLine("Operation succesful");
+                }
+                else
+                {
+                    Console.WriteLine("Note not found!");
+                }
+            }
+            else if (menu == 0)
+            {
+                break;
+                
+            }
+
+        } while (true);
     }
 }
